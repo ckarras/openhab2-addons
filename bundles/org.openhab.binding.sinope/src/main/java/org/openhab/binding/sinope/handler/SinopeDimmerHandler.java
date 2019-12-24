d8f7f0494f91d53a10eadf1c98602847544964a8
Christos Karras <ck@christoskarras.com>
2019-12-24 12:31:26 -0500

    Initial implementation for Sinope DM2500RF dimmer

---
+++ bundles/org.openhab.binding.sinope/src/main/java/org/openhab/binding/sinope/handler/SinopeDimmerHandler.java
@@ -0,0 +1,168 @@
+/**
+ * Copyright (c) 2010-2019 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.sinope.handler;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.smarthome.config.core.Configuration;
+import org.eclipse.smarthome.core.library.types.QuantityType;
+import org.eclipse.smarthome.core.library.unit.SmartHomeUnits;
+import org.eclipse.smarthome.core.thing.*;
+import org.eclipse.smarthome.core.thing.binding.BaseThingHandler;
+import org.eclipse.smarthome.core.types.Command;
+import org.openhab.binding.sinope.SinopeBindingConstants;
+import org.openhab.binding.sinope.internal.config.SinopeConfig;
+import org.openhab.binding.sinope.internal.core.SinopeDataReadRequest;
+import org.openhab.binding.sinope.internal.core.SinopeDataWriteRequest;
+import org.openhab.binding.sinope.internal.core.appdata.SinopeOutputIntensityData;
+import org.openhab.binding.sinope.internal.core.base.SinopeDataAnswer;
+import org.openhab.binding.sinope.internal.util.ByteUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+/**
+ * The {@link SinopeDimmerHandler} is responsible for handling commands, which are
+ * sent to one of the channels.
+ *
+ * @author Christos Karras - Initial contribution
+ */
+@NonNullByDefault
+public class SinopeDimmerHandler extends BaseThingHandler {
+
+    private static final int DATA_ANSWER = 0x0A;
+
+    private Logger logger = LoggerFactory.getLogger(SinopeDimmerHandler.class);
+
+    private byte[] deviceId = new byte[0];
+
+    public SinopeDimmerHandler(Thing thing) {
+        super(thing);
+    }
+
+    @Override
+    public void handleCommand(ChannelUID channelUID, Command command) {
+
+        try {
+            if (SinopeBindingConstants.CHANNEL_DIMMER_OUTPUTINTENSITY.equals(channelUID.getId()) && command instanceof QuantityType) {
+                setDimmerOutputIntensity(((QuantityType<?>) command).intValue());
+            }
+        } catch (IOException e) {
+            logger.debug("Cannot handle command for channel {} because of {}", channelUID.getId(),
+                    e.getLocalizedMessage());
+            this.getSinopeGatewayHandler().setCommunicationError(true);
+        }
+    }
+
+    public void setDimmerOutputIntensity(int outputIntensity) throws IOException {
+        this.getSinopeGatewayHandler().stopPoll(); // We are about to send something to gateway.
+        try {
+            if (this.getSinopeGatewayHandler().connectToBridge()) {
+                logger.debug("Connected to bridge");
+
+                SinopeDataWriteRequest req = new SinopeDataWriteRequest(this.getSinopeGatewayHandler().newSeq(), deviceId,
+                        new SinopeOutputIntensityData());
+                ((SinopeOutputIntensityData) req.getAppData()).setOutputIntensity(outputIntensity);
+
+                SinopeDataAnswer answ = (SinopeDataAnswer) this.getSinopeGatewayHandler().execute(req);
+
+                if (answ.getStatus() == DATA_ANSWER) {
+                    logger.debug("Output intensity is now: {} %%", outputIntensity);
+                } else {
+                    logger.debug("Cannot set output intensity, status: {}", answ.getStatus());
+                }
+            } else {
+                logger.debug("Could not connect to bridge to update Output Intensity");
+                updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.COMMUNICATION_ERROR, "Cannot connect to bridge");
+            }
+        } finally {
+            this.getSinopeGatewayHandler().schedulePoll();
+        }
+
+    }
+
+    @Override
+    public void bridgeStatusChanged(ThingStatusInfo bridgeStatusInfo) {
+        logger.debug("bridgeStatusChanged {}", bridgeStatusInfo);
+        updateDeviceId();
+    }
+
+    @Override
+    public void initialize() {
+        logger.debug("initializeThing thing {}", getThing().getUID());
+        updateDeviceId();
+    }
+
+    @Override
+    protected void updateConfiguration(Configuration configuration) {
+        super.updateConfiguration(configuration);
+        updateDeviceId();
+    }
+
+    public void updateDimmerOutputIntensity(int outputIntensity) {
+        updateState(SinopeBindingConstants.CHANNEL_DIMMER_OUTPUTINTENSITY, new QuantityType<>(outputIntensity, SmartHomeUnits.PERCENT));
+    }
+
+    public void update() throws IOException {
+        if (this.deviceId != null) {
+            if (isLinked(SinopeBindingConstants.CHANNEL_DIMMER_OUTPUTINTENSITY)) {
+                this.updateDimmerOutputIntensity(readOutputIntensity());
+            }
+        } else {
+            logger.error("Device id is null for Thing UID: {}", getThing().getUID());
+            updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.CONFIGURATION_ERROR);
+        }
+    }
+
+    private int readOutputIntensity() throws UnknownHostException, IOException {
+        SinopeDataReadRequest req = new SinopeDataReadRequest(this.getSinopeGatewayHandler().newSeq(), deviceId,
+                new SinopeOutputIntensityData());
+        logger.debug("Reading Output Intensity for device id: {}", ByteUtil.toString(deviceId));
+        SinopeDataAnswer answ = (SinopeDataAnswer) this.getSinopeGatewayHandler().execute(req);
+        int intensity = ((SinopeOutputIntensityData) answ.getAppData()).getOutputIntensity();
+        logger.debug("Output intensity is : {} %", intensity);
+        return intensity;
+    }
+
+    private void updateDeviceId() {
+        String sDeviceId = (String) getConfig().get(SinopeBindingConstants.CONFIG_PROPERTY_DEVICE_ID);
+        this.deviceId = SinopeConfig.convert(sDeviceId);
+        if (this.deviceId == null) {
+            logger.debug("Invalid Device id, cannot convert id: {}", sDeviceId);
+            updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.CONFIGURATION_ERROR, "Invalid Device id");
+            return;
+        }
+        Bridge bridge = this.getBridge();
+        if (bridge == null) {
+            updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.BRIDGE_OFFLINE);
+            return;
+        }
+        SinopeGatewayHandler handler = getSinopeGatewayHandler();
+        if (handler != null) {
+            handler.registerDimmerHandler(this);
+        }
+        updateStatus(ThingStatus.ONLINE);
+    }
+
+    private @Nullable SinopeGatewayHandler getSinopeGatewayHandler() {
+        Bridge bridge = this.getBridge();
+        if (bridge != null) {
+            return (SinopeGatewayHandler) bridge.getHandler();
+        } else {
+            return null;
+        }
+    }
+
+}
